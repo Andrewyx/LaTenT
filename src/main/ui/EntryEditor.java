afb4d5630ec50fbd94da993cef03deacb742cffd
Andrew Mao <andrewyxmao@gmail.com>
2024-02-11 01:52:44 -0800

    basic ui

--- src/main/ui/EntryEditor.java
+++ src/main/ui/EntryEditor.java
@@ -2,11 +2,14 @@ package ui;
 
 import model.Entry;
 
+import java.util.Scanner;
+
 /**
  * Opens Editor interface for creating or modifying entries
  */
 public class EntryEditor {
     private boolean editorState;
+    private Scanner userInput;
     private Entry activeEntry;
 
     /**
@@ -14,12 +17,14 @@ public class EntryEditor {
      */
     public EntryEditor() {
         this.editorState = true;
+        this.userInput = new Scanner(System.in);
     }
 
     /**
+     * MODIFIES: this
      * EFFECTS: Runs editor processes and processes user input
      */
-    private void runEditor() {
+    public void runEditor(Entry entry) {
         while (this.editorState) {
 
         }
@@ -30,5 +35,45 @@ public class EntryEditor {
      */
     private void deleteEntry() {
         //stub
+    }
+
+
+    /**
+     *  MODIFIES: this
+     *  EFFECTS: Runs the create new entry process and processes user input
+     */
+    public Entry runCreateEntry() {
+        String title;
+        String command;
+        String description;
+        while (this.editorState) {
+            System.out.println("Enter new Entry Title: ");
+            title = this.userInput.nextLine();
+
+            System.out.println("Enter new Entry Command: ");
+            command = this.userInput.nextLine();
+
+            System.out.println("Enter new Entry Description: ");
+            description = this.userInput.nextLine();
+            if (confirmChoice()) {
+                return new Entry(title, description, command);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * EFFECTS: produces true if the user enters confirm
+     */
+    private boolean confirmChoice() {
+        while (true) {
+            System.out.println("Type <YES> to confirm choice, or <q> to exit");
+            String userText = this.userInput.next().toLowerCase();
+            if (userText.equals("yes")) {
+                return true;
+            } else if (userText.equals("q")) {
+                return false;
+            }
+        }
     }
 }
--- src/main/ui/LaTenTApp.java
+++ src/main/ui/LaTenTApp.java
@@ -1,6 +1,7 @@
 package ui;
 
 import model.Catalogue;
+import model.Entry;
 
 import java.util.Scanner;
 
@@ -10,7 +11,9 @@ import java.util.Scanner;
 public class LaTenTApp {
     private boolean appState;
     private String userInputString;
-    private Catalogue catalogue;
+    protected Catalogue catalogue;
+    private Scanner userInput;
+    private EntryEditor editor;
 
     /**
      * EFFECTS: Starts new instance of the app and runs it, with a new catalogue
@@ -19,6 +22,7 @@ public class LaTenTApp {
         this.appState = true;
         this.userInputString = null;
         this.catalogue = new Catalogue();
+        this.userInput = new Scanner(System.in);
         this.runApp();
     }
 
@@ -27,30 +31,94 @@ public class LaTenTApp {
      * EFFECTS: processes the users commands
      */
     private void runApp() {
-
+        String inputText = null;
+        System.out.println(
+                "\n"
+                        + "                               \n"
+                        + " __        _____         _____ \n"
+                        + "|  |   ___|_   _|___ ___|_   _|\n"
+                        + "|  |__| .'| | | | -_|   | | |  \n"
+                        + "|_____|__,| |_| |___|_|_| |_|  \n"
+                        + "                               \n"
+        );
         while (this.appState) {
-            //do stub
+            System.out.println("Welcome to LaTent");
+            System.out.println("Enter your command");
+            inputText = this.userInput.next();
+            inputText.toString().toLowerCase();
+
+            if (inputText.equals("q")) {
+                this.appState = false;
+            } else {
+                System.out.println("Running: " + inputText);
+                this.handleInput(inputText);
+            }
         }
     }
 
     /**
+     * MODIFIES: this
+     * EFFECTS: reads and processes user input
+     */
+    private void handleInput(String input) {
+        switch (input) {
+            case "/a":
+                System.out.println("Creating Entry");
+                this.newEntry();
+                break;
+            case "/f":
+                System.out.println("Finding Entry");
+                this.findEntry();
+                break;
+            case "/o":
+                System.out.println("Editing Entry");
+                this.openEntry();
+                break;
+        }
+    }
+
+    /**
+     * MODIFIES: this
      * EFFECTS: creates new entry to add to catalogue
      */
     private void newEntry() {
-
+        System.out.println("Opening Entry Creator");
+        this.editor = new EntryEditor();
+        Entry createdEntry = this.editor.runCreateEntry();
+        if (createdEntry != null) {
+            this.catalogue.addEntry(createdEntry);
+        }
     }
 
     /**
      * EFFECTS: Displays the command info from the catalogue
      */
     private void findEntry() {
-
+        System.out.println("Give Entry Command");
+        userInputString = this.userInput.next();
+        if (this.catalogue.hasEntry(userInputString)) {
+            Entry activeEntry = this.catalogue.getCatalogueEntry(userInputString);
+            System.out.println(
+                    activeEntry.getTitle()
+                            + activeEntry.getCommand()
+                            + activeEntry.getDescription()
+            );
+        } else {
+            System.out.println("Entry Not Found");
+        }
     }
 
     /**
-     * EFFECTS:
+     * MODIFIES: this
+     * EFFECTS: Opens the given entry in the editor
      */
     private void openEntry() {
-
+        System.out.println("Give Entry Command to Open");
+        userInputString = this.userInput.next();
+        this.catalogue.getCatalogueEntry(userInputString);
+        this.editor = new EntryEditor();
+        this.editor.runEditor(
+                this.catalogue.getCatalogueEntry(userInputString)
+        );
     }
 }
